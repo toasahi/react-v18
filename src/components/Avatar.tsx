17b87705a35b5e0caaa0fbd32d30e4f3d793e3bc
toasahi <asa1313to@gmail.com>
2022-10-31 10:51:18 +0900

    update:Section3 Transition create TaskList

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,27 @@
+import {ReactNode} from "react";
+
+type Props = {
+    children: ReactNode;
+    isSelected?: boolean;
+    onClick: (assignee: string) => void;
+}
+
+export const Avatar = (({children, isSelected = false, onClick}: Props) => {
+    const border = isSelected ? '3px solid orange' : '1px solid gray'
+    return (
+        <div style={{
+            width: '30px',
+            height: '30px',
+            border,
+            borderRadius: '50%',
+            textAlign: 'center',
+            lineHeight: '30px',
+            userSelect: 'none',
+            background: 'white',
+            color: '#333',
+            margin: '5px'
+        }} onClick={() => onClick(`${children}`)}>
+            {`${children}`}
+        </div>
+    )
+})
